7b82da73e44db98bab39d9d7d036d2b5b5b30e59
霽月 <102911089+Se1getsu@users.noreply.github.com>
2023-05-08 14:52:16 +0900

    feat: insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,23 @@
+﻿# 挿入ソート
+# 平均計算量 O(n²) 最悪計算量 O(n²)
+
+
+def insertion_sort(seq):
+    for i in range(1, len(seq)):
+        tmp = seq[i]
+        for j in range(i, 0, -1):
+            if seq[j-1] <= tmp: break
+            seq[j] = seq[j-1]
+            j -= 1
+        seq[j] = tmp
+        print("%4d"%tmp, seq)
+    return seq
+
+
+import random
+a = [random.randint(0,100) for _ in range(10)]
+print("Input\n%s\n" % a)
+
+insertion_sort(a)
+
+print("Fin")
